10f5684193ce23d933b6552f8fda3735ebf12948
ADB9DZZ_mmm <ADB9DZZ@mmm.com>
2024-08-07 12:38:38 +0530

    Add project files.

---
+++ MagicWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MagicWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
